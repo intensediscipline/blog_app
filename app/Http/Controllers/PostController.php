486e8755a1ded6423fd9fb731c36c4da8a4c851a
intensediscipline <47402690+intensediscipline@users.noreply.github.com>
2024-03-19 16:25:44 +0000

    Added middleware policy check
    Used middleware policy check on route instead of checking it in the post controller delete method

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -9,11 +9,7 @@ use Illuminate\Http\Request;
 class PostController extends Controller
 {
     public function delete(Post $post) {
-        if (auth()->user()->cannot('delete', $post)) {
-            return "You cannot delete this post";
-        }
         $post->delete();
-
         return redirect('/profile/' . auth()->user()->username)->with('success', 'Post successfully deleted.');
     }
 
