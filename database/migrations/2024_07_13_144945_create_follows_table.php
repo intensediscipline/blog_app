86757b6e2d47ed86036d311ce9aba2086d37c415
intensediscipline <47402690+intensediscipline@users.noreply.github.com>
2024-07-13 17:36:59 +0100

    Follow functionality
    Added ability for user to follow other users

---
+++ database/migrations/2024_07_13_144945_create_follows_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('follows', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained();
+            // following 2 lines do the same as the line above
+            $table->unsignedBigInteger('followeduser');
+            $table->foreign('followeduser')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('follows');
+    }
+};
